43f5a82a7907cef777d6db5ea1fa89ca5ae3a24d
mate.biro <mate.biro@icellmobilsoft.hu>
2024-03-13 16:10:23 +0100

    #48 - test fix

--- dookug-testsuite/dookug-ts-document/dookug-ts-document-service/src/test/java/hu/icellmobilsoft/dookug/ts/document/rest/generate/PostDocumentGenerateInlineMultipartIT.java
+++ dookug-testsuite/dookug-ts-document/dookug-ts-document-service/src/test/java/hu/icellmobilsoft/dookug/ts/document/rest/generate/PostDocumentGenerateInlineMultipartIT.java
@@ -70,9 +70,7 @@ class PostDocumentGenerateInlineMultipartIT extends AbstractGenerateDocumentIT {
                 .baseUri(URI.create(documentBaseUri))
                 .build(IDocumentGenerateInlineInternalRestClient.class);
         DocumentGenerateRequest request = generateRequestBuilder.fullFillHandlebarsPdfBoxDatabase();
-        request.getGeneratorSetup()
-                .setParametersData(
-                        templateParameterDataFromFile(FileUtil.readFileFromResource(DocumentServiceTestConstant.PDF_BOX_TEMPLATE_PARAMETERS)));
+        request.getGeneratorSetup().setParametersData(templateParameterDataFromFile(DocumentServiceTestConstant.PDF_BOX_TEMPLATE_PARAMETERS));
         DocumentGenerateMultipartForm form = new DocumentGenerateMultipartForm();
         form.setRequest(request);
         ByteArrayInputStream bis = new ByteArrayInputStream(FileUtil.readFileFromResource(DocumentServiceTestConstant.PDF_BOX_TEMPLATE).getBytes());
